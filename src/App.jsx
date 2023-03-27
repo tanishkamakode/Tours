a93ef1417108fd39945ae91772c517cc98ced7ff
Tanishka <makodetanishka@gmail.com>
2023-03-27 18:52:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Tours from "./Tours";
+import Loading from "./Loading";
+
+const url = "https://course-api.com/react-tours-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState(true);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // ToDo
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
+};
+export default App;
